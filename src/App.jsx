41d3b1433e77ef48c481d9c5fa0672c7e58f2ee7
carlclayton-sh <carl.clayton@simplyhealth.co.uk>
2025-09-17 14:06:42 +0100

    button props

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,11 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button
+          className="button--primary"
+          onClick={() => setCount((count) => count + 1)}
+          disabled={count >= 10}
+        >
           count is {count}
         </button>
         <p>
